0fa8b24aa0a9e8650eb05ecc759ad1234ce1cc9a
aCzsy <40326158+aCzsy@users.noreply.github.com>
2020-11-09 21:26:17 +0000

    Update findEvenIndex.java

--- Codewars_Code/src/com/magnet/findEvenIndex.java
+++ Codewars_Code/src/com/magnet/findEvenIndex.java
@@ -14,24 +14,8 @@ public class findEvenIndex {
         int result = -1;
         for (int i = 0; i <= arr.length - 1; i++) {
             int[] left = new int[i], right = new int[(arr.length - 1) - i];
-
-            /** System.arraycopy parameters :
-             * System.arraycopy(src,srsPos,dest,destPos,length);
-             *  src − This is the source array.
-                srcPos − This is the starting position in the source array.
-                dest − This is the destination array.
-                destPos − This is the starting position in the destination data.
-                length − This is the number of array elements to be copied.
-             */
             System.arraycopy(arr, 0, left, 0, i);
             System.arraycopy(arr, i + 1, right, 0, (arr.length - 1) - i);
-//            System.out.println(getSumOfArray(left));
-//            System.out.println(getSumOfArray(right));
-//            System.out.println("Current Index: " + arr[i]);
-//            System.out.println("Left:");
-//            printArray(left);
-//            System.out.println("Right:");
-//            printArray(right);
             if (getSumOfArray(left) == getSumOfArray(right)) {
                 result = i;
             }
@@ -41,7 +25,6 @@ public class findEvenIndex {
     }
 
     private static int getSumOfArray (int[] arr) {
-//        return Arrays.stream(arr).sum();
         Integer sum = 0;
         for(int i = 0; i < arr.length;i++){
             sum += arr[i];
